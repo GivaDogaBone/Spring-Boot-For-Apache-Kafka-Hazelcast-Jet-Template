5385dc61b4fe8386b83e8fc6e1a36e52524afdb6
GivaDogaBone <g_a_quintas@yahoo.com>
2022-09-13 18:30:31 +1000

    Initial Commit

---
+++ src/main/java/com/example/springbootforapachekafkahazelcasttemplate/SpringBootForApacheKafkaHazelcastTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootforapachekafkahazelcasttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForApacheKafkaHazelcastTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootForApacheKafkaHazelcastTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootforapachekafkahazelcasttemplate/SpringBootForApacheKafkaHazelcastTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootforapachekafkahazelcasttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootForApacheKafkaHazelcastTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
